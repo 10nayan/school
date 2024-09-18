85a487dd2d951df44dac22db63228f3dc4206eda
10nayan <nayanjyotipatowary@gmail.com>
2024-09-18 23:57:56 +0530

    Serializer added for REST API'

--- core/views.py
+++ core/views.py
@@ -1,3 +1,20 @@
+from django.db.models.manager import BaseManager
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Department, SchoolClass, Subject
+from .serializers import DepartmentSerializer, SchoolClassSerializer, SubjectSerializer
 
-# Create your views here.
+
+class DepartmentViewset(viewsets.ModelViewSet):
+    serializer_class = DepartmentSerializer
+    queryset = Department.objects.all()
+
+
+class SubjectViewset(viewsets.ModelViewSet):
+    serializer_class = SubjectSerializer
+    queryset: BaseManager[Subject] = Subject.objects.all()
+
+
+class SchoolClassViewset(viewsets.ModelViewSet):
+    serializer_class = SchoolClassSerializer
+    queryset = SchoolClass.objects.all()
